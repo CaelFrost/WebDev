278ca14e1f793ae5474d8d816fc2fd3f7dbd3caf
CaelFrost <steve.stuart@fluidimaging.com>
2013-11-03 20:40:33 -0500

    V2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 /* Author:
 
 */
-
+$(document).ready(function() {
+   $('#red,#blue,#green,#yellow').mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });
+   $('#red,#blue,#green,#yellow').mouseleave(function() {
+       $(this).animate({
+           height: '-=10px'
+       }); 
+   });
+   $('#red,#blue,#green,#yellow').click(function() {
+       $(this).toggle(1000);
+   }); 
+});
 
 
 
